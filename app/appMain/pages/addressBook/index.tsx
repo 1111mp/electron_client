7e2a7ce78ceaf34d06ad46024d4e987b7070219d
张逸凡 <zhangyifan@zhangyifandeMacBook-Pro.local>
2020-12-09 22:00:20 +0800

    开始开发通讯录
    Signed-off-by: 张逸凡 <zhangyifan@zhangyifandeMacBook-Pro.local>

--- app/appMain/pages/addressBook/index.tsx
+++ app/appMain/pages/addressBook/index.tsx
@@ -1,11 +1,18 @@
 import './styles.scss';
 
 import React from 'react';
+import Groups from './groups';
 
 export type Props = {};
 
 const AddressBook: React.ComponentType<Props> = () => {
-  return <div>address book</div>;
+  return (
+    <div className="module-addressbook">
+      <div className="module-addressbook-list">
+        <Groups />
+      </div>
+    </div>
+  );
 };
 
 export default AddressBook;
